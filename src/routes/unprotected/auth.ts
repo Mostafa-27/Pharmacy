7e6c88e4a33d045085636099af0d8e2de15b1a3b
Mostafa-27 <101284489+Mostafa-27@users.noreply.github.com>
2025-01-26 14:07:49 +0200

    finish signup and sigin in

--- src/routes/unprotected/auth.ts
+++ src/routes/unprotected/auth.ts
@@ -3,35 +3,64 @@ import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import {
+  comparePassword,
+  createJWT,
+  hashPassword,
+} from "../../middleware/authMiddleware";
 const router = Router();
 const prisma = new PrismaClient();
 
 router.post("/signup", async (req, res) => {
   const { email, password, name, role, phone_number, location } = req.body;
-  const password_hash = await bcrypt.hash(password, 10);
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password_hash,
-      name,
-      role,
-      phone_number,
-      location,
-    },
-  });
-  res.json(user);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: email,
+        name: name,
+        role: role,
+        phone_number: phone_number,
+        location: location,
+        password_hash: await hashPassword(password),
+      },
+    });
+    const token = createJWT({ id: user.user_id, username: user.email });
+    res.status(201).json({ token: token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
-  const user = await prisma.user.findUnique({ where: { email } });
 
-  const token = jwt.sign(
-    { userId: user.user_id, role: user.role },
-    process.env.JWT_SECRET as string,
-    { expiresIn: "1h" }
-  );
-  res.json({ token });
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      res.status(401).json({ message: "user not found" });
+      return;
+    }
+
+    const isValid = await comparePassword(password, user.password_hash);
+
+    if (!isValid) {
+      res
+        .status(401)
+        .json({ message: "invalid password" + user.password_hash });
+      return;
+    }
+
+    const token = createJWT({ id: user.user_id, username: user.email });
+    res.status(200).json({ token: token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 export default router;
